4759eb40934897a3051af165a7f6344c3054b919
pewpowder <itaaaami@mail.ru>
2024-08-17 19:53:03 +0700

    add our-service block

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './js/header'
+import './js/our-service'
 
 import './styles/main.scss'
 import './styles/header.scss'
 import './styles/immerse-vr.scss'
 import './styles/about-us.scss'
+import './styles/our-service.scss'
---
+++ src/js/our-service.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+$(document).ready(function(){
+    $('.our-service__card-link').click(function (e) {
+        e.preventDefault();
+    });
+});
